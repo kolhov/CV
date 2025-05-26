927a8ee0bd68263525536b534cbfd0d79d626f79
kolhov <kolhovv@ya.ru>
2025-05-26 22:46:04 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import LayoutMain from "@/components/layout/LayoutMain.vue";
+</script>
+
+<template>
+  <div class="flex justify-center items-center w-full h-full">
+    <LayoutMain />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/layout/LayoutMain.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import {Card} from "@/components/ui/card";
+</script>
+
+<template>
+  <main class="flex flex-col w-2/3 mt-8">
+    <card class="items-center">
+      asdasd
+    </card>
+    <card class="">
+      asdasd
+    </card>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
