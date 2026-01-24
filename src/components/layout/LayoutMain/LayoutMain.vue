f3a77cdae225e6dfce20752bd40d1189606458bf
kolhov <kolhovv@gmail.com>
2026-01-24 12:33:41 +0300

    update

--- src/components/layout/LayoutMain/LayoutMain.vue
+++ src/components/layout/LayoutMain/LayoutMain.vue
@@ -1,48 +1,57 @@
 <script setup lang="ts">
-import {Card} from "@/components/ui/card";
-import NavigationBar from "@/components/layout/NavigationBar/NavigationBar.vue";
-import {storeToRefs} from "pinia";
-import {useLocaleStore} from "@/stores/locale-store.ts";
-import {Accordion, AccordionTrigger, AccordionItem, AccordionContent} from "@/components/ui/accordion";
-import {Icon} from "@iconify/vue";
-import ProjectCard from "@/components/ui/ProjectCard/ProjectCard.vue";
-import SectionHead from "@/components/ui/SectionHead/SectionHead.vue";
-import {Button} from "@/components/ui/button";
+import NavigationBar from '@/components/layout/NavigationBar/NavigationBar.vue'
+import { storeToRefs } from 'pinia'
+import { useLocaleStore } from '@/stores/locale-store.ts'
+import {
+  Accordion,
+  AccordionTrigger,
+  AccordionItem,
+  AccordionContent,
+} from '@/components/ui/accordion'
+import { Icon } from '@iconify/vue'
+import ProjectCard from '@/components/ui/ProjectCard/ProjectCard.vue'
+import SectionHead from '@/components/ui/SectionHead/SectionHead.vue'
+import { Button } from '@/components/ui/button'
 
-const {locale, currLang} = storeToRefs(useLocaleStore());
-
+const { locale, currLang } = storeToRefs(useLocaleStore())
 </script>
 
 <template>
   <main class="flex sm:flex-row flex-col mt-8 m-x-5 w-full justify-center">
-    <NavigationBar class="mr-8 p-2"/>
+    <NavigationBar class="mr-8 p-2" />
     <div class="flex flex-col m-2 p-2" v-if="locale">
-      <section id="intro" class="mb-8 pt-8 justify-items-center">
-        <h1 class="text-xl">{{locale.name}}</h1>
+      <section id="intro" class="flex flex-col items-center mb-8 pt-8 justify-center">
+        <h1 class="text-xl">{{ locale.name }}</h1>
         <a :href="`/files/CV_${currLang.toUpperCase()}.pdf`" download>
-        <Button class="mt-6 cursor-pointer font-semibold bg-green-400 hover:bg-green-300 shadow-md shadow-green-400/50 hover:shadow-green-300/50">
-          <Icon icon="carbon:generate-pdf" class="w-full h-full"/>{{locale.page.pdf}}
-        </Button>
+          <Button
+            class="mt-6 cursor-pointer font-semibold bg-green-400 hover:bg-green-300 shadow-md shadow-green-400/50 hover:shadow-green-300/50"
+          >
+            <Icon icon="carbon:generate-pdf" class="w-full h-full" />{{ locale.page.pdf }}
+          </Button>
         </a>
       </section>
       <section id="work" class="mb-12 max-w-148">
         <SectionHead :title="locale.page.work" />
-        <div class="flex max-w-150 w-full pb-6" v-for="job in locale.jobs">
+        <div class="flex max-w-150 w-full pb-6" v-for="job in locale.jobs" :key="job.months">
           <div class="flex flex-col w-1/4 min-w-28">
-            <span class="text-sm text-muted-foreground pt-1 tracking-wide">{{job.year}}</span>
-            <span class="text-sm text-muted-foreground">{{job.months}}</span>
+            <span class="text-sm text-muted-foreground pt-1 tracking-wide">{{ job.year }}</span>
+            <span class="text-sm text-muted-foreground">{{ job.months }}</span>
           </div>
           <div class="w-10" />
           <div class="flex flex-col w-3/4">
-            <span class="text-lg">{{job.position}}</span>
-            <span class="text-muted-foreground">{{job.company}}</span>
+            <span class="text-lg">{{ job.position }}</span>
+            <span class="text-muted-foreground">{{ job.company }}</span>
             <ul v-if="job.achievements.length > 1">
-              <li class="flex flex-row py-0.5 text-sm text-muted-foreground " v-for="x in job.achievements">
-                <Icon class="mt-1 min-w-4 max-w-4" icon="lucide:chevron-right" /> {{x}}
+              <li
+                class="flex flex-row py-0.5 text-sm text-muted-foreground"
+                v-for="x in job.achievements"
+                :key="x[0]"
+              >
+                <Icon class="mt-1 min-w-4 max-w-4" icon="lucide:chevron-right" /> {{ x }}
               </li>
             </ul>
-            <span v-else class="py-0.5 text-sm text-muted-foreground ">
-              {{job.achievements[0]}}
+            <span v-else class="py-0.5 text-sm text-muted-foreground">
+              {{ job.achievements[0] }}
             </span>
           </div>
         </div>
@@ -51,21 +60,31 @@ const {locale, currLang} = storeToRefs(useLocaleStore());
         <SectionHead :title="locale.page.education" />
         <div class="flex">
           <div class="flex flex-col relative w-full gap-1">
-            <img src="/img/logo_web_fpf.png" alt="Logo FPF SLU" class="w-[55%] place-self-center mb-6 sm:w-[25%] sm:absolute sm:top-0 sm:right-0">
-            <span class="text-lg">{{locale.uni.name}}</span>
-            <span class="text-sm text-muted-foreground mt-2">{{locale.uni.year}}</span>
-            <span class="text-md text-muted-foreground">{{locale.uni.program}}</span>
-            <span class="text-md text-muted-foreground">{{locale.uni.specialization}}</span>
+            <img
+              src="/img/logo_web_fpf.png"
+              alt="Logo FPF SLU"
+              class="w-[55%] place-self-center mb-6 sm:w-[25%] sm:absolute sm:top-0 sm:right-0"
+            />
+            <span class="text-lg">{{ locale.uni.name }}</span>
+            <span class="text-sm text-muted-foreground mt-2">{{ locale.uni.year }}</span>
+            <span class="text-md text-muted-foreground">{{ locale.uni.program }}</span>
+            <span class="text-md text-muted-foreground">{{ locale.uni.specialization }}</span>
           </div>
         </div>
         <div>
           <Accordion type="single" class="w-full mt-4" collapsible default-value="item-1">
             <AccordionItem value="item-1">
-              <AccordionTrigger class="py-2 cursor-pointer"><h3>{{locale.page.courses}}</h3></AccordionTrigger>
+              <AccordionTrigger class="py-2 cursor-pointer"
+                ><h3>{{ locale.page.courses }}</h3></AccordionTrigger
+              >
               <AccordionContent class="pl-2">
                 <ul>
-                  <li class="flex flex-row py-0.5 text-sm text-muted-foreground" v-for="x in locale.courses">
-                    <Icon class="m-1" icon="lucide:check-check"/>{{x}}
+                  <li
+                    class="flex flex-row py-0.5 text-sm text-muted-foreground"
+                    v-for="(x, i) in locale.courses"
+                    :key="i"
+                  >
+                    <Icon class="m-1" icon="lucide:check-check" />{{ x }}
                   </li>
                 </ul>
               </AccordionContent>
@@ -75,60 +94,68 @@ const {locale, currLang} = storeToRefs(useLocaleStore());
       </section>
       <section id="projects" class="mb-18 max-w-148">
         <SectionHead :title="locale.page.projects" />
-        <div class="grid grid-cols-5 grid-rows-14  gap-4 min-h-280">
-          <ProjectCard class="row-span-7  col-span-5"
-                       :project="locale.projects[0]"
-                       img-placeholder="/img/cloud1.png"
-                       gif-on-hover="/img/cloud2.gif"
+        <div class="grid grid-cols-5 grid-rows-14 gap-4 min-h-280">
+          <ProjectCard
+            class="row-span-7 col-span-5"
+            :project="locale.projects[0]"
+            img-placeholder="/img/cloud1.png"
+            gif-on-hover="/img/cloud2.gif"
           />
-          <ProjectCard class="border rounded-md col-span-2 row-span-3 "
-                       :project="locale.projects[1]"
-                       img-placeholder="/img/poe1.png"
-                       gif-on-hover="/img/poe2.gif"
+          <ProjectCard
+            class="border rounded-md col-span-2 row-span-3"
+            :project="locale.projects[1]"
+            img-placeholder="/img/poe1.png"
+            gif-on-hover="/img/poe2.gif"
           />
-          <ProjectCard class="border rounded-md col-span-3 row-span-5 "
-                       :project="locale.projects[3]"
-                       img-placeholder="/img/dpb0.png"
-                       gif-on-hover="/img/dpb.gif"
+          <ProjectCard
+            class="border rounded-md col-span-3 row-span-5"
+            :project="locale.projects[3]"
+            img-placeholder="/img/dpb0.png"
+            gif-on-hover="/img/dpb.gif"
           />
-          <ProjectCard class="border rounded-md col-span-2 row-span-2 "
-                       :project="locale.projects[2]"
+          <ProjectCard
+            class="border rounded-md col-span-2 row-span-2"
+            :project="locale.projects[2]"
           />
           <div class="border rounded-md col-span-5 row-span-1">
-            <a class="flex flex-row hover:underline w-full h-full justify-center items-center" :href="locale.github" target="_blank">
-              <Icon icon="octicon:mark-github-16" class="place-self-center min-w-4"/>
-              <span class="pl-2">{{locale.page.gitAd}}</span>
+            <a
+              class="flex flex-row hover:underline w-full h-full justify-center items-center"
+              :href="locale.github"
+              target="_blank"
+            >
+              <Icon icon="octicon:mark-github-16" class="place-self-center min-w-4" />
+              <span class="pl-2">{{ locale.page.gitAd }}</span>
             </a>
           </div>
         </div>
       </section>
-      <section id="contacts" class="h-160">
+      <section id="contacts" class="h-180">
         <SectionHead :title="locale.page.contacts" />
         <span class="flex flex-row m-2">
-          <Icon icon="tdesign:location" class="place-self-center min-w-4"/>
-          <span class="pl-2">{{locale.location}}</span>
+          <Icon icon="tdesign:location" class="place-self-center min-w-4" />
+          <span class="pl-2">{{ locale.location }}</span>
         </span>
         <a class="flex flex-row hover:underline m-2 w-1/3" :href="locale.github" target="_blank">
-          <Icon icon="octicon:mark-github-16" class="place-self-center min-w-4"/>
-          <span class="pl-2">{{locale.github.split('://')[1]}}</span>
+          <Icon icon="octicon:mark-github-16" class="place-self-center min-w-4" />
+          <span class="pl-2">{{ locale.github.split('://')[1] }}</span>
         </a>
-        <a class="flex flex-row hover:underline m-2 w-1/2 pt-4" href="https://t.me/faantanka" target="_blank">
-          <Icon icon="ph:telegram-logo" class="place-self-center min-w-4"/>
-          <span class="pl-2">{{locale.page.telegram}}</span>
+        <a
+          class="flex flex-row hover:underline m-2 w-1/2 pt-4"
+          href="https://t.me/faantanka"
+          target="_blank"
+        >
+          <Icon icon="ph:telegram-logo" class="place-self-center min-w-4" />
+          <span class="pl-2">{{ locale.page.telegram }}</span>
         </a>
         <a class="flex flex-row hover:underline m-2 w-1/3" :href="`mailto:${locale.email}`">
-          <Icon icon="lucide:mail" class="place-self-center min-w-4"/>
-          <span class="pl-2">{{locale.email}}</span>
+          <Icon icon="lucide:mail" class="place-self-center min-w-4" />
+          <span class="pl-2">{{ locale.email }}</span>
         </a>
       </section>
     </div>
-    <div v-else>
-      Loading...
-    </div>
+    <div v-else>Loading...</div>
     <div class="xl:min-w-48" />
   </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/layout/NavigationBar/NavigationBar.vue
+++ src/components/layout/NavigationBar/NavigationBar.vue
@@ -1,84 +1,91 @@
 <script setup lang="ts">
-import {storeToRefs} from "pinia";
-import {useLocaleStore} from "@/stores/locale-store.ts";
-import LangButton from "@/components/layout/NavigationBar/LangButton.vue";
-import {debounce} from "@/lib/utils.ts";
+import { storeToRefs } from 'pinia'
+import { useLocaleStore } from '@/stores/locale-store.ts'
+import LangButton from '@/components/layout/NavigationBar/LangButton.vue'
+import { debounce } from '@/lib/utils.ts'
 
-const {locale} = storeToRefs(useLocaleStore());
+const { locale } = storeToRefs(useLocaleStore())
 
 function scrollCalc() {
-  let mainNavLinks = document.querySelectorAll('.xx a');
-  let offsetPx = 24;
+  const mainNavLinks = document.querySelectorAll('.xx a')
+  const offsetPx = 24
 
   mainNavLinks.forEach((link) => {
-    const anchor = link as HTMLAnchorElement;
-    if (anchor.hash == '') return;
+    const anchor = link as HTMLAnchorElement
+    if (anchor.hash == '') return
 
-    let section = document.querySelector(anchor.hash);
-    if (section == null) return;
+    const section = document.querySelector(anchor.hash)
+    if (section == null) return
 
-    let el = (section as HTMLElement).getBoundingClientRect();
-    if (
-      el.top <= window.innerHeight &&
-      el.bottom > offsetPx
-    ) {
-      anchor.classList.add('current');
-      let allCurrents = document.querySelectorAll('.current');
-      let allFirsts = document.querySelector('.first');
+    const el = (section as HTMLElement).getBoundingClientRect()
+    if (el.top <= window.innerHeight && el.bottom > offsetPx) {
+      anchor.classList.add('current')
+      const allCurrents = document.querySelectorAll('.current')
+      if (!allCurrents?.length) anchor.classList.add('current')
+
+      const allFirsts = document.querySelector('.first')
       if (allFirsts != null) {
-        allFirsts.classList.remove('first');
+        allFirsts.classList.remove('first')
       }
-      allCurrents[0].classList.add('first');
+      allCurrents[0].classList.add('first')
     } else {
-      anchor.classList.remove('current');
+      anchor.classList.remove('current')
     }
-  });
+  })
 }
 
 window.addEventListener('load', () => {
-  window.onscroll = debounce(scrollCalc, 5);
+  window.onscroll = debounce(scrollCalc, 5)
   setTimeout(() => {
-    scrollCalc();
-  }, 100);
+    scrollCalc() 
+  }, 100)
 
-  // Smooth navigation links
-  let anchorLinks = document.querySelectorAll('a[href^="#"]');
-  for (let item of anchorLinks) {
+  const anchorLinks = document.querySelectorAll('a[href^="#"]')
+  anchorLinks.forEach((item) => {
     item.addEventListener('click', (e) => {
-      let hashVal = item.getAttribute('href');
-      let target = document.querySelector(hashVal!);
-      if (target) target.scrollIntoView({
-        behavior: 'smooth',
-      });
-      history.pushState(null, '', hashVal);
-      e.preventDefault();
-    });
-  }
-});
+      e.preventDefault()
+      const hashVal = item.getAttribute('href')
+      const target = document.querySelector(hashVal!)
+      if (target)
+        target.scrollIntoView({
+          behavior: 'smooth',
+        })
+      history.pushState(null, '', hashVal)
+    })
+  })
+})
 </script>
 
 <template>
   <div class="min-w-48">
-    <div class="flex flex-col gap-1 sm:fixed inset-x-0 sm:left-auto sm:right-auto sm:top-20 xx items-center sm:items-start" v-if="locale" role="navigation">
+    <div
+      class="flex flex-col gap-1 sm:fixed inset-x-0 sm:left-auto sm:right-auto sm:top-20 xx items-center sm:items-start"
+      v-if="locale"
+      role="navigation"
+    >
       <LangButton />
-      <a href="#intro" class="link">{{locale.page.intro}}</a>
-      <a href="#work" class="link">{{locale.page.work}}</a>
-      <a href="#education" class="link">{{locale.page.education}}</a>
-      <a href="#projects" class="link">{{locale.page.projects}}</a>
-      <a href="#contacts" class="link">{{locale.page.contacts}}</a>
-      <br>
-      <a :href="`mailto:${locale.email}`" class="contact">{{locale.email}}</a>
-      <a :href="locale.github" target="_blank" class="contact">{{locale.github.split('://')[1]}}</a>
+      <a href="#intro" class="link">{{ locale.page.intro }}</a>
+      <a href="#work" class="link">{{ locale.page.work }}</a>
+      <a href="#education" class="link">{{ locale.page.education }}</a>
+      <a href="#projects" class="link">{{ locale.page.projects }}</a>
+      <a href="#contacts" class="link">{{ locale.page.contacts }}</a>
+      <br />
+      <a :href="`mailto:${locale.email}`" class="contact">{{ locale.email }}</a>
+      <a :href="locale.github" target="_blank" class="contact">{{
+        locale.github.split('://')[1]
+      }}</a>
     </div>
   </div>
 </template>
 
 <style scoped>
-.xx {
-  a {
-    transition: min-width 0.3s, border 0.3s, padding 0.3s, box-shadow 0.3s,
+.xx a {
+  transition:
+    min-width 0.3s,
+    border 0.3s,
+    padding 0.3s,
+    box-shadow 0.3s,
     background 0.15s;
-  }
 }
 
 .first {
